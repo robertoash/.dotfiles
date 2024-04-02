073d5da1f243b0ff103878fa55b45749e6db1e56
rash <j.roberto.ash@gmail.com>
2024-04-02 11:24:07 +0200

    Snap floating window to corner with mouse

---
+++ scripts/hyprland/snap_window_to_corner.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import json
+import subprocess
+
+
+def run_command(command):
+    try:
+        return subprocess.check_output(
+            command, shell=True, stderr=subprocess.STDOUT
+        ).decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        return e.output.decode()
+
+
+def run_command_json(command):
+    output = run_command(command)
+    try:
+        return json.loads(output)
+    except json.JSONDecodeError:
+        return None
+
+
+def get_cursor_position():
+    output = run_command_json("hyprctl cursorpos -j")
+    return output["x"], output["y"]
+
+
+def get_active_window_info():
+    return run_command_json("hyprctl activewindow -j") or {}
+
+
+def get_window_geometry(window_info):
+    return {
+        "x": window_info["at"][0],
+        "y": window_info["at"][1],
+        "width": window_info["size"][0],
+        "height": window_info["size"][1],
+    }
+
+
+def move_window_to_corner(corner):
+
+    for direction in corner:
+        run_command(f"hyprctl dispatch movewindow {direction}")
+
+
+if __name__ == "__main__":
+    click_x, click_y = get_cursor_position()
+    window_info = get_active_window_info()
+    if window_info:
+        geometry = get_window_geometry(window_info)
+        window_center_x = geometry["x"] + geometry["width"] // 2
+        window_center_y = geometry["y"] + geometry["height"] // 2
+
+        # Determine closest corner of the window based on the click position
+        if click_x < window_center_x and click_y < window_center_y:
+            corner = ["u", "l"]
+        elif click_x >= window_center_x and click_y < window_center_y:
+            corner = ["u", "r"]
+        elif click_x < window_center_x and click_y >= window_center_y:
+            corner = ["d", "l"]
+        else:
+            corner = ["d", "r"]
+
+        was_pinned = "pinned" in window_info and window_info["pinned"]
+        if was_pinned:
+            run_command("hyprctl dispatch pin")
+
+        move_window_to_corner(corner)
+
+        if was_pinned:
+            run_command("hyprctl dispatch pin")
