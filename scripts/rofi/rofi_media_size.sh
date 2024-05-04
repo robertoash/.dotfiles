b04b9c5f0ecf29ca0019cd008a7ee3abe6e154be
rash <j.roberto.ash@gmail.com>
2024-05-04 09:25:20 +0200

    media_size script dev

--- scripts/rofi/rofi_media_size.sh
+++ scripts/rofi/rofi_media_size.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Connect to offlinelab and get directory sizes
-ssh offlinelab "du --all --max-depth=1 -h /media/offline_data/data/media/films/ | \
-sort -rh | awk 'NR==1 {sub(/\\/media\\/offline_data\\/data\\/media\\/films\\//, \
-\"TOTAL\"); print; next} {gsub(/\\/media\\/offline_data\\/data\\/media\\/films\\//, \
-\"\"); print}'" | \
+ssh offlinelab "du --all --max-depth=2 -h /media/offline_data/data/media/ | sort -rh | awk 'NR==1 {sub(/\\/media\\/offline_data\\/data\\/media\\//, \"\"); printf \"%-6s %-s\\n\", \"TOTAL\", \$1; next} {gsub(/\\/media\\/offline_data\\/data\\/media\\//, \"\"); gsub(/[ ()]/, \"_\"); \$2 = tolower(\$2); gsub(/__+/, \"_\", \$2); gsub(/_$/, \"\", \$2); printf \"%-6s %-s\\n\", \$1, \$2}'" | \
 # Display results in a rofi menu
 rofi -theme catppuccin_mocha_single_column -dmenu -i -threads 0 -width 100 \
 -p "media_size:"
