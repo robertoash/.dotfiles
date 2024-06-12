77fa20792174a9edd3ab6d73e73e82e570b83296
rash <j.roberto.ash@gmail.com>
2024-06-12 16:22:15 +0200

    Fixed stop_history to not purge history but just stop recording

--- zsh/zsh_functions/shell_history.zsh
+++ zsh/zsh_functions/shell_history.zsh
@@ -1,13 +1,13 @@
 # Stop shell history recording
-stop_history() {
-  HISTSIZE=0
-  SAVEHIST=0
-  echo "History recording stopped."
+stp_history() {
+  function zshaddhistory() {
+    return 1
+  }
+  echo 'History recording stopped.'
 }
 
 # Start shell history recording
-start_history() {
-  HISTSIZE=10000  # Adjust as desired
-  SAVEHIST=10000  # Adjust as desired
-  echo "History recording started."
+strt_history() {
+  unset -f zshaddhistory
+  echo 'History recording started.'
 }
