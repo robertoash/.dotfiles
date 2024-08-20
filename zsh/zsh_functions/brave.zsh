fdda4e516be8c468657d0ba067a40101f06e7854
rash <j.roberto.ash@gmail.com>
2024-08-20 13:31:32 +0200

    Make brave_zen able to open any buku bookmark via fzf, index or string search

--- zsh/zsh_functions/brave.zsh
+++ zsh/zsh_functions/brave.zsh
@@ -1,7 +1,35 @@
 brave_zen() {
-  if [[ "$1" != https://* ]]; then
-    brave --app="https://$1"
+  if [ -z "$1" ]; then
+      brave --app=$(bk_o --url)
+      echo "Opening Buku Fuzzy Search"
   else
-    brave --app="$1"
+    case $1 in
+      --buku)
+        shift
+        if [ -z "$1" ]; then
+            brave --app=$(bk_o --url)
+            echo "Opening Buku Fuzzy Search"
+        else
+            brave --app=$(bk_o --url "$1")
+            echo "Opening Buku index $1"
+        fi
+        return
+        ;;
+      [0-9]*)
+        brave --app=$(bk_o --url "$1")
+        echo "Opening Buku index $1"
+        return
+        ;;
+      https://*|http://*)
+        brave --app="$1"
+        echo "Opening $1"
+        return
+        ;;
+      *)
+        brave --app="https://$1"
+        echo "Opening https://$1"
+        return
+        ;;
+    esac
   fi
 }
