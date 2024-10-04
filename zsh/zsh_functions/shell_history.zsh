cdffc005705481e300a43b2e131348048a8c1ad1
rash <j.roberto.ash@gmail.com>
2024-10-04 13:38:44 +0200

    Disable resh history

--- zsh/zsh_functions/shell_history.zsh
+++ zsh/zsh_functions/shell_history.zsh
@@ -3,11 +3,27 @@ stp_history() {
   function zshaddhistory() {
     return 1
   }
+
+  # Stop resh daemon
+  resh-daemon-stop
+
+  # Set an environment variable to disable resh hooks
+  # Had to modify __resh_preexec and __resh_precmd
+  # on ~/.resh/hooks.sh to check for this variable
+  # and exit early if it is set.
+  export RESH_HISTORY_DISABLED=1
+
   echo 'History recording stopped.'
 }
 
 # Start shell history recording
 strt_history() {
   unset -f zshaddhistory
+
+  resh-daemon-start
+
+  # Unset the environment variable
+  unset RESH_HISTORY_DISABLED
+
   echo 'History recording started.'
 }
