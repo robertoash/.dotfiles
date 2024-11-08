46f51ed96a9ea2ac60b2a28d11d6718a5ff53c68
rash <j.roberto.ash@gmail.com>
2024-11-08 10:53:53 +0100

    Bigger rofi media size

--- scripts/rofi/rofi_media_size.sh
+++ scripts/rofi/rofi_media_size.sh
@@ -3,5 +3,5 @@
 # Connect to offlinelab and get directory sizes
 ssh offlinelab "du --all --max-depth=2 -h /media/offline_data/data/media/ | sort -rh | awk 'NR==1 {sub(/\\/media\\/offline_data\\/data\\/media\\//, \"\"); printf \"%-6s %-s\\n\", \"TOTAL\", \$1; next} {gsub(/\\/media\\/offline_data\\/data\\/media\\//, \"\"); gsub(/[ ()]/, \"_\"); \$2 = tolower(\$2); gsub(/__+/, \"_\", \$2); gsub(/_$/, \"\", \$2); printf \"%-6s %-s\\n\", \$1, \$2}'" | \
 # Display results in a rofi menu
-rofi -theme catppuccin_mocha_single_column -dmenu -i -threads 0 -width 100 \
+rofi -theme catppuccin_mocha_single_column -dmenu -i -threads 0 -width 100 -lines 50 \
 -p "media_size:"
