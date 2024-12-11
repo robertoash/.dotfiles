20d5f5b5fc6077cc45b5ed10d0f9715591999e81
rash <j.roberto.ash@gmail.com>
2024-12-11 12:07:19 +0100

    Make stop_history quiet

--- zsh/zsh_functions/shell_history.zsh
+++ zsh/zsh_functions/shell_history.zsh
@@ -1,6 +1,6 @@
 # Stop shell history recording
 stp_history() {
-  function zshaddhistory() {
+  zshaddhistory() {
     return 1
   }
 
@@ -16,6 +16,10 @@ stp_history() {
   # Redirect _Z_DATA to /dev/null to prevent z from creating a new .z file
   export _Z_DATA=/dev/null
 
+  # Silencing permission denied errors from z plugin
+  unalias zshz 2>/dev/null || true
+  function zshz { command zshz "$@" 2>/dev/null; }
+
   echo 'History recording stopped.'
 }
 
@@ -30,6 +34,10 @@ strt_history() {
 
   # Set _Z_DATA to enable z to update its database again
   export _Z_DATA=~/.config/z/.z
+
+  # Restore original z command
+  unfunction zshz 2>/dev/null || true
+  source ~/.config/zsh/plugins/zsh-z/zsh-z.plugin.zsh
 
   echo 'History recording started.'
 }
