4f77723c81ca5ed02c1e1d9b650be61e43b7a61f
rash <j.roberto.ash@gmail.com>
2025-01-16 14:03:43 +0100

    Qutebrowser config init

---
+++ qutebrowser/config.py
@@ -0,0 +1,7 @@
+config.load_autoconfig(False)
+
+c.auto_save.session = True
+c.editor.command = ["alacritty", "-e", "helix", "{file}:{line}:{column0}"]
+c.url.default_page = "about:empty"
+c.url.start_pages = ["about:empty"]
+c.url.searchengines = {"DEFAULT": "https://search.brave.com/search?q={}&source=desktop"}
