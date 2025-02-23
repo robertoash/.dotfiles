838cd077dd1f10c5381e8e243915ca697f3294ac
rash <j.roberto.ash@gmail.com>
2025-02-23 01:11:23 +0100

    Get rid of resh to keep history

--- zsh/zsh_functions/shell_history.zsh
+++ zsh/zsh_functions/shell_history.zsh
@@ -4,15 +4,6 @@ stp_history() {
     return 1
   }
 
-  # Stop resh daemon
-  resh-daemon-stop
-
-  # Set an environment variable to disable resh hooks
-  # Had to modify __resh_preexec and __resh_precmd
-  # on ~/.resh/hooks.sh to check for this variable
-  # and exit early if it is set.
-  export RESH_HISTORY_DISABLED=1
-
   # Redirect _Z_DATA to /dev/null to prevent z from creating a new .z file
   export _Z_DATA=/dev/null
 
@@ -26,11 +17,6 @@ stp_history() {
 # Start shell history recording
 strt_history() {
   unset -f zshaddhistory
-
-  resh-daemon-start
-
-  # Unset the environment variable
-  unset RESH_HISTORY_DISABLED
 
   # Set _Z_DATA to enable z to update its database again
   export _Z_DATA=~/.config/z/.z
