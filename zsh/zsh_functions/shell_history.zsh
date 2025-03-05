32b7ab8e2447f3897009035f8ddc2fc5b61ec991
rash <j.roberto.ash@gmail.com>
2025-03-05 11:26:04 +0100

    Replace zoxide with fasd

--- zsh/zsh_functions/shell_history.zsh
+++ zsh/zsh_functions/shell_history.zsh
@@ -4,7 +4,6 @@ trap 'start_history' EXIT
 stop_history() {
     # Store original paths
     export _HISTFILE_ORIG="${HISTFILE:-$HOME/.config/zsh/.zsh_history}"
-    export _ZO_DATA_DIR_ORIG="${_ZO_DATA_DIR:-$HOME/.config/zoxide}"
 
     # Use the same directory as the original history file
     local hist_dir
@@ -13,9 +12,8 @@ stop_history() {
     export HISTFILE="$hist_dir/.zsh_history_tmp_$$"
     touch "$HISTFILE"
 
-    # Create a temp dir under the original zoxide directory
-    export _ZO_DATA_DIR="$_ZO_DATA_DIR_ORIG/temp/"
-    mkdir -p "$_ZO_DATA_DIR"  # Ensure temp dir exists
+    # Stop fasd history
+    export _FASD_RO="no"
 
     echo "Shell history stopped..."
 }
@@ -23,7 +21,6 @@ stop_history() {
 start_history() {
     # Retrieve original values
     local main_histfile="${_HISTFILE_ORIG:-$HOME/.config/zsh/.zsh_history}"
-    local original_zoxide_dir="${_ZO_DATA_DIR_ORIG:-$HOME/.config/zoxide}"
     local tmp_histfile="$HISTFILE"
 
     # Only delete if HISTFILE is a temporary file (to avoid deleting real history)
@@ -32,13 +29,11 @@ start_history() {
     fi
 
     export HISTFILE="$main_histfile"  # Restore original history
-    export _ZO_DATA_DIR="$original_zoxide_dir"  # Restore zoxide data directory
-
-    rm -rf "$original_zoxide_dir/temp/"  # Clean up temp directory
 
     # Unset temporary variables since they are no longer needed
     unset _HISTFILE_ORIG
-    unset _ZO_DATA_DIR_ORIG
-    
+    unset _FASD_RO
+    #unset _ZO_DATA_DIR_ORIG
+
     echo "Shell history started..."
 }
