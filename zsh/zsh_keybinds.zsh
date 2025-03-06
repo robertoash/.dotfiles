b2ef2627d36677298535afde57133e4d0dc58e3e
rash <j.roberto.ash@gmail.com>
2025-03-06 23:46:59 +0100

    Keybind for fasd autocomplete

--- zsh/zsh_keybinds.zsh
+++ zsh/zsh_keybinds.zsh
@@ -7,6 +7,11 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey '^R' fzf_history_widget
 
+# Fasd completion widgets
+bindkey '^X^A' fasd-complete    # C-x C-a to do fasd-complete (files and directories)
+bindkey '^X^F' fasd-complete-f  # C-x C-f to do fasd-complete-f (only files)
+bindkey '^X^D' fasd-complete-d  # C-x C-d to do fasd-complete-d (only directories)
+
 # Alt + LeftArrow -> backward-word
 bindkey "^[[1;3D" backward-word
 # Alt + RightArrow -> forward-word
