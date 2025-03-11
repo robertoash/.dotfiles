810e15d7b0754ffd6f84fdc375d8e9a201593e07
rash <j.roberto.ash@gmail.com>
2025-03-11 21:12:48 +0100

    Adapt to asdf breaking changes

--- scripts/mqtt/launch_mqtt_services.sh
+++ scripts/mqtt/launch_mqtt_services.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Make logging_utils available
+# Activate the dedicated venv for MQTT services
+source /home/rash/.config/scripts/mqtt/venv/bin/activate
+
+# Set PYTHONPATH to ensure logging_utils is available
 export PYTHONPATH="$PYTHONPATH:/home/rash/.config/scripts"
 
-# Ensure the current directory is where .envrc is located
-cd /home/rash/.config/scripts/mqtt || exit
+# Ensure we are in the correct working directory
+cd /home/rash/.config/scripts/mqtt || exit 1
 
-# Source same path as .zshrc
-source /home/rash/.config/zsh/.zsh_path
-
-# ASDF
-. /opt/asdf-vm/asdf.sh
-
-eval "$(direnv export bash)"
+# Manually source .envrc to load secrets (since systemd won't trigger direnv)
+source /home/rash/.config/scripts/mqtt/.envrc
 
 # Initialize debug argument
 debug_arg=""
 
-# Check if --debug is provided in either argument
+# Check if --debug is provided
 if [[ "$1" == "--debug" ]]; then
     debug_arg="--debug"
     service="$2"
@@ -28,7 +26,7 @@ else
     service="$1"
 fi
 
-# Determine which service to start based on the argument
+# Determine which service to start
 if [[ "$service" == "mqtt_reports" ]]; then
     python3 /home/rash/.config/scripts/mqtt/mqtt_reports.py $debug_arg
 elif [[ "$service" == "mqtt_listener" ]]; then
