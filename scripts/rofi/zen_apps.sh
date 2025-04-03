122b598f445a4cf7f8a16c3adb5bbe2b27155bc0
rash <j.roberto.ash@gmail.com>
2025-04-03 02:59:19 +0200

    Replace keybinds with rofi script

---
+++ scripts/rofi/zen_apps.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+choices="🧠 ChatGPT
+🔍 Perplexity
+🎞️ SVT Play
+🎬 Max
+🎵 YouTube
+🧿 Overseerr
+🧬 GitHub
+📺 CNN"
+
+choice=$(echo "$choices" | rofi -dmenu -i -p "Launch App:")
+
+launch() {
+  brave --new-window \
+    --enable-features=UseOzonePlatform \
+    --ozone-platform=wayland \
+    --profile-directory="AppProfile" \
+    --app="$1"
+}
+
+trigger() {
+  python3 ~/.config/scripts/secrets/launch_iptv.py --"$1"
+}
+
+case "$choice" in
+  "🧠 ChatGPT") launch "https://chatgpt.com/" ;;
+  "🔍 Perplexity") launch "https://perplexity.ai" ;;
+  "🎞️ SVT Play") launch "https://svtplay.se" ;;
+  "🎬 Max") launch "https://max.com" ;;
+  "🎵 YouTube") launch "https://www.youtube.com" ;;
+  "🧿 Overseerr") launch "https://watchlist.rashlab.net" ;;
+  "🧬 GitHub") launch "https://github.com" ;;
+  "📺 CNN") trigger "cnn" ;;
+  *) exit 1 ;;
+esac
