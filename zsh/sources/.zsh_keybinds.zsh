9a5d905bec267959e86bed3eee6eead94d9eb95d
rash <j.roberto.ash@gmail.com>
2025-04-14 23:16:08 +0200

    Fix fzf path autocomplete
    yes

--- zsh/sources/.zsh_keybinds.zsh
+++ zsh/sources/.zsh_keybinds.zsh
@@ -14,6 +14,8 @@ bindkey '^X^A' fasd-complete    # C-x C-a to do fasd-complete (files and directo
 bindkey '^X^F' fasd-complete-f  # C-x C-f to do fasd-complete-f (only files)
 bindkey '^X^D' fasd-complete-d  # C-x C-d to do fasd-complete-d (only directories)
 
+# Alt + F -> Fuzzy path completion
+bindkey '^[f' __fzf_path_complete
 # Alt + LeftArrow -> backward-word
 bindkey "^[[1;3D" backward-word
 # Alt + RightArrow -> forward-word
