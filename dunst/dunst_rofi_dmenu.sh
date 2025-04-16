c7f80c1ea82d5cf56e5da2d50d04fcff932a331f
rash <j.roberto.ash@gmail.com>
2025-04-16 18:43:36 +0200

    Use rofi for dunst context

---
+++ dunst/dunst_rofi_dmenu.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/usr/bin/rofi -theme /home/rash/.config/rofi/current_theme_single_column.rasi -dmenu -i -threads 0 -width 100 -theme-str 'entry { placeholder: "dunst action...";}'
