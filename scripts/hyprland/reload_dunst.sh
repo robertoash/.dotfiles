5ba1f6d37a1656223109409ed77e5353765301e7
rash <j.roberto.ash@gmail.com>
2025-04-16 18:41:38 +0200

    Reorganize systemd user services

--- scripts/hyprland/reload_dunst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Kill all dunst instances
-killall dunst
-# Start dunst with the custom config file
-dunst -conf /home/rash/.config/dunst/dunst.config &
-# Notify of the reload
-dunstify "Dunst has been reloaded." --icon=dialog-information --timeout=5000
