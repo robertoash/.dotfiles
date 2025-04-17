29b7a71d156f7dc7542cc8676351f595e511565f
rash <j.roberto.ash@gmail.com>
2025-04-17 21:45:23 +0200

    Hardcoded size and used proper dispatchers

--- scripts/hyprland/lagom_floating.py
+++ scripts/hyprland/lagom_floating.py
@@ -3,6 +3,9 @@
 import json
 import subprocess
 
+new_width = "1280"
+new_height = "720"
+
 
 # Function to execute a shell command and return the output
 def run_command(cmd):
@@ -15,19 +18,11 @@ active_window_json = run_command("hyprctl activewindow -j")
 active_window = json.loads(active_window_json)
 window_id = active_window.get("address")
 floating = active_window.get("floating")
-monitor = active_window.get("monitor")
-
-if monitor == 1:
-    new_width = "900"
-    new_height = "500"
-else:
-    new_width = "1280"
-    new_height = "720"
 
 if floating:
     # Float the window and resize
-    run_command("hyprctl dispatch togglefloating")
+    run_command("hyprctl dispatch settiled")
 else:
     # Float the window and resize
-    run_command("hyprctl dispatch togglefloating")
+    run_command("hyprctl dispatch setfloating")
     run_command(f"hyprctl dispatch resizeactive exact {new_width} {new_height}")
