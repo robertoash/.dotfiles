2a43c33ca3f8bf8322b3c2b7983367f9cf415e11
rash <j.roberto.ash@gmail.com>
2025-04-24 01:06:53 +0200

    Get rid of unused scripts
    Other

--- scripts/rofi/rofi_hyprkeys.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-hyprkeys -br -c ~/.config/hypr/keybinds.conf | \
-rofi -theme ~/.config/rofi/current_theme_single_column.rasi \
--dmenu -i -threads 0 -width 100 -theme-str 'entry { placeholder: "hyprkeys filter..."; }'
--- scripts/rofi/rofi_media_size.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Connect to offlinelab and get directory sizes
-ssh offlinelab "du --all --max-depth=2 -h /media/offline_data/data/media/ | sort -rh | awk 'NR==1 {sub(/\\/media\\/offline_data\\/data\\/media\\//, \"\"); printf \"%-6s %-s\\n\", \"TOTAL\", \$1; next} {gsub(/\\/media\\/offline_data\\/data\\/media\\//, \"\"); gsub(/[ ()]/, \"_\"); \$2 = tolower(\$2); gsub(/__+/, \"_\", \$2); gsub(/_$/, \"\", \$2); printf \"%-6s %-s\\n\", \$1, \$2}'" | \
-# Display results in a rofi menu
-rofi -theme ~/.config/rofi/current_theme_single_column.rasi -dmenu -i -threads 0 -width 100 -lines 50 \
--theme-str 'entry { placeholder: "media_size..."; }'
