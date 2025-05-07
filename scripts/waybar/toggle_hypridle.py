9cc1bb7caf82965e397a30472408cf1f41e0c618
rash <j.roberto.ash@gmail.com>
2025-05-08 00:55:33 +0200

    Manual idle_inhibit via keybind

---
+++ scripts/waybar/toggle_hypridle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+import signal
+import subprocess
+from pathlib import Path
+
+PID_FILE = Path("/tmp/waybar/hypridle.pid")
+STATUS_FILE = Path("/tmp/waybar/hypridle_status.json")
+
+
+def write_status(hypridle_is_on: bool):
+    # ⚫ = running, 🔴 = off: because we want to show idle_inhibit status in waybar
+    STATUS_FILE.write_text("⚫" if hypridle_is_on else "🔴")
+
+
+def get_status():
+    if PID_FILE.exists():
+        return True
+    else:
+        return False
+
+
+def toggle_hypridle(hypridle_is_on: bool):
+    if hypridle_is_on:
+        try:
+            os.kill(int(PID_FILE.read_text()), signal.SIGTERM)
+        except ProcessLookupError:
+            pass
+        PID_FILE.unlink(missing_ok=True)
+        return False
+    else:
+        proc = subprocess.Popen(
+            ["hypridle"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+        PID_FILE.write_text(str(proc.pid))
+        return True
+
+
+def main():
+    hypridle_is_on = get_status()
+    hypridle_is_on = toggle_hypridle(hypridle_is_on)
+    write_status(hypridle_is_on)
+
+
+if __name__ == "__main__":
+    main()
