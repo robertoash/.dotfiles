3796b03b1816f4b860f1324fe952193890cc3db4
rash <j.roberto.ash@gmail.com>
2025-05-11 20:09:47 +0200

    File browser in nvim when opening a dir

--- zsh/zsh_functions/nvim.zsh
+++ zsh/zsh_functions/nvim.zsh
@@ -1,8 +1,13 @@
 nv() {
   if [ -d "$1" ]; then
-    nvim -c "cd $(printf '%q' "$1")"
+    nvim -c "cd $(printf '%q' "$1")" -c "Telescope file_browser hidden=true grouped=true"
   else
     nvim "$@"
   fi
+}
+
+# Open the cwd in nvim
+nvd() {
+  nvim -c "Telescope file_browser hidden=true grouped=true"
 }
 
