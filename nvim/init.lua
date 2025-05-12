22bb2a8b90a1552049bbfaea35dddd18c8fd2997
rash <j.roberto.ash@gmail.com>
2025-05-12 20:01:11 +0200

    Modified nvim mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -354,6 +354,7 @@ require("lazy").setup({
 						hijack_netrw = true,
 						grouped = true,
 						hidden = true,
+						hide_parent_dir = true,
 						mappings = {
 							["i"] = {
 								["<A-c>"] = fb_actions.create,
@@ -1051,25 +1052,22 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", "<cmd>echo 'Use h to move!!'<CR>")
+vim.keymap.set("n", "<right>", "<cmd>echo 'Use l to move!!'<CR>")
+vim.keymap.set("n", "<up>", "<cmd>echo 'Use k to move!!'<CR>")
+vim.keymap.set("n", "<down>", "<cmd>echo 'Use j to move!!'<CR>")
+
+-- Navigation
+vim.keymap.set("n", "<C-j>", "10j", { desc = "Move down 10 times" })
+vim.keymap.set("n", "<C-k>", "10k", { desc = "Move up 10 times" })
 
 -- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
+--  Use CTRL+Shift+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+vim.keymap.set("n", "<C-s-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-s-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-s-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-s-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Open file_browser
 vim.keymap.set("n", "<leader>sb", ":Telescope file_browser<CR>", { desc = "[S]earch with file [B]rowser" })
