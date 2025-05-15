73ef43a0237f2985a54d4b00a0c8ae081bc48e7c
rash <j.roberto.ash@gmail.com>
2025-05-15 21:38:16 +0200

    Simplify nv wrapper in shell

--- zsh/zsh_functions/nvim.zsh
+++ zsh/zsh_functions/nvim.zsh
@@ -1,19 +1,6 @@
 # Common implementation function
-_nvim_wrapper() {
-  if [ -d "$1" ]; then
-    command nvim "$1" -c "Telescope find_files"
-  else
-    command nvim "$@"
-  fi
-}
-
-# Define both nvim and nv using the common implementation
-nvim() {
-  _nvim_wrapper "$@"
-}
-
 nv() {
-  _nvim_wrapper "$@"
+    command nvim "$@"
 }
 
 # Open the cwd in nvim with Telescope file_browser
