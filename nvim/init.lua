18a9f3a13eca852398fc46ce98d37fe538bbb8f8
rash <j.roberto.ash@gmail.com>
2025-05-22 10:25:15 +0200

    Neovide config

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,22 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 --=============================================================================
+-- NEOVIDE SETUP
+--=============================================================================
+if vim.g.neovide then
+    vim.g.neovide_padding_top = 20
+	vim.g.neovide_padding_bottom = 10
+	vim.g.neovide_padding_right = 20
+	vim.g.neovide_padding_left = 20
+	vim.g.neovide_opacity = 0.8
+	vim.g.neovide_normal_opacity = 0.8
+	vim.g.neovide_cursor_animation_length = 0.150
+
+
+end
+
+
+--=============================================================================
 -- PLUGINS
 --=============================================================================
 require("lazy").setup({
