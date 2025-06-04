7f91314cc9fe5533699e735c9fa069c2f64da388
rash <j.roberto.ash@gmail.com>
2025-06-05 00:15:28 +0200

    Change cargo_update_all fish function

--- fish/functions/cargo_update_all.fish
+++ fish/functions/cargo_update_all.fish
@@ -1,4 +1,3 @@
-function cargo_update_all --wraps="cargo install --list | grep '^[^ ]' | cut -d' ' -f1 | xargs -n1 cargo install --force" --description "alias cargo_update_all cargo install --list | grep '^[^ ]' | cut -d' ' -f1 | xargs -n1 cargo install --force"
-  cargo install --list | grep '^[^ ]' | cut -d' ' -f1 | xargs -n1 cargo install --force $argv
-        
+function cargo_update_all --wraps='cargo-install-update install-update --all' --description 'cargo-install-update install-update --all'
+    cargo-install-update install-update --all
 end
