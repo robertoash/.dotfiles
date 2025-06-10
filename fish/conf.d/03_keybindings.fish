50a296e4d5dfa33c8737397216652ce4c7791b44
rash <j.roberto.ash@gmail.com>
2025-06-10 13:56:06 +0200

    Shellgpt fish integration

--- fish/conf.d/03_keybindings.fish
+++ fish/conf.d/03_keybindings.fish
@@ -84,14 +84,7 @@ for mode in insert default visual
     bind -M $mode \cd insert_zoxide_dir
 end
 
-# History substring search (if fish_history_substring_search is installed)
-# bind \e\[A history-substring-search-up
-# bind \e\[B history-substring-search-down
-
-# Note: Fish doesn't have direct equivalents for some zsh-specific bindings like:
-# - fasd-complete functions (fasd isn't commonly used with fish)
-# - sgpt bindings (would need custom fish functions)
-# - fzf path completion (fish has built-in completion system)
-
-# FZF bindings (if fzf is installed)
-# These are typically handled by fzf's fish integration
+# Shell-GPT integration - Alt+G
+for mode in insert default visual
+    bind -M $mode \eg sgpt_fish
+end
