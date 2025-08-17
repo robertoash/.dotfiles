4b7c638abd15a41a1cb7fd61b9c7073def00a8fb
rash <j.roberto.ash@gmail.com>
2025-08-17 02:55:12 +0200

    Add go to fish path

--- fish/conf.d/01_path.fish
+++ fish/conf.d/01_path.fish
@@ -1,8 +1,11 @@
 # ~/.config/fish/conf.d/01_path.fish
 # Path Configuration
 
-# Add Cargo path
+# Add Cargo pkgs to path
 fish_add_path "$HOME/.cargo/bin"
+
+# Add Go pkgs to path
+fish_add_path (go env GOPATH)/bin
 
 # Add local bin path
 fish_add_path "/home/rash/.local/bin"
