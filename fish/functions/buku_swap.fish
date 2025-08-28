6a48728aab35880812705c7f3d585ec2669a8168
rash <j.roberto.ash@gmail.com>
2025-08-28 12:45:34 +0200

    Remane to buku_swap

---
+++ fish/functions/buku_swap.fish
@@ -0,0 +1,3 @@
+function buku_swap
+    python3 ~/.config/scripts/shell/switch_buku.py $argv
+end
--- fish/functions/switch_buku_db.fish
+++
@@ -1,3 +0,0 @@
-function switch_buku_db
-    python3 "$HOME/.config/scripts/shell/switch_buku.py" $argv
-end
