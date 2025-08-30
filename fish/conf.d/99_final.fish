dd1d82ab5d416eaef8e9fa8a34a6ced9cb678d5a
rash <j.roberto.ash@gmail.com>
2025-08-30 22:22:03 +0200

    Switch back to buku rash on reboot, shutdown or long inactivity

--- fish/conf.d/99_final.fish
+++ fish/conf.d/99_final.fish
@@ -11,10 +11,3 @@ if status is-interactive && not set -q YAZI_LEVEL
     command fastfetch
 end
 
-# Auto-switch back to rash database on shell exit to ensure rashp is encrypted
-function __buku_exit_handler --on-event fish_exit
-    # Only run if not in secure shell (secure shell has its own handler)
-    if not set -q SECURE_SHELL
-        python3 ~/.config/scripts/shell/switch_buku.py rash 2>/dev/null
-    end
-end
