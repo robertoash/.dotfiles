b398d290a61fe13aed7163446af9b30d3124dbb9
rash <rash@dotfiles.local>
2025-12-28 08:47:52 +0100

    Remove zsh config

--- linuxmini/config/zsh/sources/.zsh_fzf_config.zsh
+++
@@ -1,5 +0,0 @@
-# Fzf configs
-# 🧠 User-friendly wrapper configs
-export FZF_CONFIG_DEFAULT="fzf --preview 'bat --color=always {}' --preview-window '~3' --color 'fg:#ffffff,fg+:#ffffff,bg:#010111,preview-bg:#010111,border:#7dcfff'"
-export FZF_CONFIG_NO_PREVIEW="fzf --color 'fg:#ffffff,fg+:#ffffff,bg:#010111,preview-bg:#010111,border:#7dcfff'"
-export FZF_PREVIEW=true
--- linuxmini/config/zsh/zsh_functions/switch_buku.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-switch_buku_db() {
-    python3 "$HOME/.config/scripts/shell/switch_buku.py" "$@"
-}
-
-# Modify the startup call to use the full path
-switch_buku_db "rash" --startup
