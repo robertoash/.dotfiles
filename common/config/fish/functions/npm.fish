7ac347d5733c78e3fef41a38bc6446dfd1c09920
rash <rash@dotfiles.local>
2026-01-02 16:53:34 +0100

    Fix npm installation for global packages

--- common/config/fish/functions/npm.fish
+++ common/config/fish/functions/npm.fish
@@ -5,6 +5,8 @@ function npm
     # Check if we've already set up npm in this session
     if not set -q __npm_setup_done
         echo "🔧 Setting up npm environment..." >&2
+        # Unset the problematic override to let ~/.npmrc prefix take effect
+        set -e NPM_CONFIG_PREFIX
         set -gx NPM_PREFIX (command npm config get prefix)
         fish_add_path "$NPM_PREFIX/bin"
         set -g __npm_setup_done 1
