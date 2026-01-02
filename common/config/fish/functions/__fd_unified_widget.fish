2abd9d87ac14f61d6dbe788f86f8f8da43dddf47
rash <rash@dotfiles.local>
2026-01-02 12:48:57 +0100

    fdf+Tab in fish

---
+++ common/config/fish/functions/__fd_unified_widget.fish
@@ -0,0 +1,34 @@
+# ~/.config/fish/functions/__fd_unified_widget.fish
+function __fd_unified_widget
+    set -l cmd (commandline -b)
+    set -l token (commandline -t)
+    set -l tokens (string split ' ' $cmd)
+
+    # Check if current token is a trigger (fdf, fdd, fda)
+    switch "$token"
+        case fdf
+            set -l result (fd -Hi --no-ignore-vcs -t f | fzf --height 40% --reverse)
+            if test -n "$result"
+                commandline -t -- (string escape "$result")
+            end
+            commandline -f repaint
+            return
+        case fdd
+            set -l result (fd -Hi --no-ignore-vcs -t d | fzf --height 40% --reverse)
+            if test -n "$result"
+                commandline -t -- (string escape "$result")
+            end
+            commandline -f repaint
+            return
+        case fda
+            set -l result (fd -Hi --no-ignore-vcs | fzf --height 40% --reverse)
+            if test -n "$result"
+                commandline -t -- (string escape "$result")
+            end
+            commandline -f repaint
+            return
+    end
+
+    # If we get here, it's not a trigger - do normal completion
+    commandline -f complete
+end
--- common/config/fish/functions/__frecent_unified_widget.fish
+++ common/config/fish/functions/__frecent_unified_widget.fish
@@ -4,6 +4,12 @@ function __frecent_unified_widget
     set -l token (commandline -t)
     set -l tokens (string split ' ' $cmd)
 
+    # Check for fd triggers first (fdf, fdd, fda)
+    if string match -q -r '^fd[fda]$' "$token"
+        __fd_unified_widget
+        return
+    end
+
     # Check if current token is a trigger (ff, dd, aa)
     switch "$token"
         case ff
