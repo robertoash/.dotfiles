7eaa95acbb05e22b8d684e45348f2492630f85ef
Roberto Ash <j.roberto.ash@gmail.com>
2026-02-20 14:52:19 +0100

    fix(fish): unset CLAUDE_CODE_OAUTH_TOKEN unless SSH'd into workmbp

--- common/config/fish/conf.d/02_claude_ssh_only.fish
+++ common/config/fish/conf.d/02_claude_ssh_only.fish
@@ -1,5 +1,10 @@
 # CLAUDE_CODE_OAUTH_TOKEN is injected globally but should only be active in
 # SSH sessions (to avoid conflicting with local claude account on each machine)
-if not set -q SSH_CONNECTION
-    set -e CLAUDE_CODE_OAUTH_TOKEN
+if status is-interactive
+    function __unset_claude_token_if_not_ssh --on-event fish_prompt
+        if not set -q SSH_CONNECTION; or test $hostname != workmbp
+            set -e CLAUDE_CODE_OAUTH_TOKEN
+        end
+        functions -e __unset_claude_token_if_not_ssh
+    end
 end
